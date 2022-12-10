293cbe32c1a43e90ed3538e3d11b552e02659f6a
Diksha Waghmarre <dikshaw44@gmail.com>
2022-12-10 19:33:25 +0530

    add assignment question

--- Js/script.js
+++ Js/script.js
@@ -8,32 +8,39 @@ console.log(2 + '2' + '2');//222
 console.log('hello' + 'world' + 89);//helloworld89
 console.log('hello' - 'world' + 89);//NaN
 console.log('hello' + 78);//hello78
-console.log('78' - 90 + '2');//NaN
+console.log('78' - 90 + '2');//-122
 console.log(2 - '2' + 90);//NaN
 console.log(89 - '90' / 2);//NaN
-console.log(2+ '2' + null);//220
+console.log(2+ '2' + null);//22null
 console.log((true == false) > 2);//false
 console.log((89  + 'hello' + 90 / 9));//89hello10
 
 //---------------------------------------------------------
 //Datatypes:
+//•	extract first five letters from a string (' gfuh ieiuei ')
 var a="gfuh ieiuei";
 console.log(a.slice(0,5));
 
+//•	get the length of a string and make it uppercase ('hduej dij')
 var b="hduej dij";
 console.log(b.length);
 console.log(b.toUpperCase());
 
+//•	take a string, make it lowercase and trim it ('   biji jdo   ')
 var c=" ASDR JBUJ  ";
 console.log(c.toLowerCase());
 var d=" biji jdo ";
 console.log(d.trim());
+
+//•	replace specified word in a string ('', '')
 console.log(d.replace("j",","));
 
+//•	random statements in implicit coercion e.g. (89  + 'hello' + 90 / 9)
 console.log(44+"Hello"+20/2+22);//44Hello1022
 console.log("66"/2 +"hii"+"null"+ null);//33hiinullnull
 console.log("66"/2 + null+"hii"+"null");//33hiinull
 
+//•	create an object for car.
 var car ={
     name:"Maruti",
     color:"black",
@@ -46,3 +53,6 @@ console.log(typeof car);
 console.log(car.color);
 console.log(Object.keys(car));
 console.log(Object.values(car));
+
+
+//•	revise type of each datatype
